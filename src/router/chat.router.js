0eb6234903b8ae022b5ed8b3741f8b46b0bf7ddd
divyeshmoraniya <hdivyesh62@gmail.com>
2026-01-31 14:59:21 +0530

    change socket connection

--- src/router/chat.router.js
+++ src/router/chat.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 
 import { ChatController } from "../controllers/index.js";
+import { dynamicAuth } from "../middlewares/auth.middlewares.js";
 
 export const chatRouter = Router();
 
-chatRouter.route("/addconversastion").post(ChatController.createConversastion);
-chatRouter.route("/getchat").get(ChatController.getChat);
+chatRouter.route("/addconversastion").post(dynamicAuth, ChatController.createConversastion);
+chatRouter.route("/getchat").get(dynamicAuth , ChatController.getChat);
