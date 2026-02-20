c9395d0aa6868af22083ec0aec2247cd4dc084c5
codebyviral <viralvaghela2641@gmail.com>
2026-02-20 10:15:16 +0530

    update Google Callback URL to take from ENV variable

--- src/config/passport-config.js
+++ src/config/passport-config.js
@@ -23,7 +23,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, done) => {
